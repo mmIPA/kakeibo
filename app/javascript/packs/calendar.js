7492bd8965e2077fb45535d79c017667da4b763b
mitsui <171986182+mmIPA@users.noreply.github.com>
2024-12-04 23:16:23 +0000

    レスポンシブ化

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,13 +1,21 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from "@fullcalendar/list";
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ],
+    plugins: [dayGridPlugin, listPlugin],
     initialView: 'dayGridMonth',
-    events: '/events'
+    events: '/events',
+    windowResize: function () { 
+          if (window.innerWidth < 991.98) {
+              calendar.changeView('listMonth');
+          } else {
+              calendar.changeView('dayGridMonth');
+          }
+      },
+    });
+  
+    calendar.render();
   });
-
-  calendar.render();
-});
